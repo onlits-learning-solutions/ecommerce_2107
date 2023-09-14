e562d290322a78d3c9fb4e6d8517c4d8945d2dba
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2023-09-14 21:52:18 +0530

    Fixes Re-setup project #10

--- ecommerce_2107/urls.py
+++ ecommerce_2107/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ecommerce project.
+URL configuration for ecommerce_2107 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
